2a97d6f2dffbfb87a711c58ee1220318eb9fbe23
wanti12 <wantiwanti156@gmail.com>
2023-10-09 11:52:02 +0700

    Day1biodata.java

---
+++ Day1_biodata.Java.java
@@ -0,0 +1,27 @@
+package day1_biodata.java;
+
+public class Day1_biodataJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("BIODATA ");
+        String nama = "wanti";
+        int umur = 18;
+        String nim = "D0223026";
+        String JenisKelamin = "perempuan";
+        String TTL = "mamuju,16 juni 2005";
+        String agama = "kristen";
+        String alamat = "papalang";
+        String prodi = "informatika";
+
+        System.out.println("nama : " + nama);
+        System.out.println("umur : " + umur);
+        System.out.println("NIM : " + nim);
+        System.out.println("jenis kelamin : " + JenisKelamin);
+        System.out.println("tempat tanggal lahir : " + TTL);
+        System.out.println("agama : " + agama);
+        System.out.println("alamat : " + alamat);
+        System.out.println("prodi : " + prodi);
+    }
+
+}
