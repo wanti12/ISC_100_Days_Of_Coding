1d60a5d236254748fbd04b9c8956105482e394af
wanti12 <wantiwanti156@gmail.com>
2023-10-10 17:34:08 +0700

    Days2TipeDataPrimitif

---
+++ Days2_tipeDataPrimitif.java
@@ -0,0 +1,26 @@
+package days2_tipedataprimitif;
+
+public class Days2_tipeDataPrimitif {
+
+    public static void main(String[] args) {
+        boolean hasil = true;
+        int nilai = 100;
+        byte umur = 18;
+        char N = '1';
+        long tinggi = 154;
+        float berat = 4.50f;
+        double luas = 45.123;
+        short panjang = 45;
+
+        System.out.println("nilai boolean = " + hasil);
+        System.out.println("nilai integer = " + nilai);
+        System.out.println("nilai byte = " + umur);
+        System.out.println("nilai char = " + N);
+        System.out.println("nilai long = " + tinggi);
+        System.out.println("nilai float = " + berat);
+        System.out.println("nilai double = " + luas);
+        System.out.println("nilai short = " + panjang);
+
+    }
+
+}
