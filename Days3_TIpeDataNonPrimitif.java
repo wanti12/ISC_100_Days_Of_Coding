67d61059bae5ed52358c891bceb0a87c295c9901
wanti12 <wantiwanti156@gmail.com>
2023-10-11 18:51:04 +0700

    Days3_TipeDataNonPrimitif

---
+++ Days3_TIpeDataNonPrimitif.java
@@ -0,0 +1,16 @@
+package days3_tipedatanonprimitif;
+
+public class Days3_TIpeDataNonPrimitif {
+
+    public static void main(String[] args) {
+        String nama = "wanti";
+        Integer a = 100;
+        Byte b = 16;
+        Character c = 'B';
+        Short d = 07;
+        Double e = 1.000;
+        Float f = 9.99f;
+        Boolean bilangan = true;
+    }
+
+}
