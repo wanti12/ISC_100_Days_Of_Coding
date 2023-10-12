b7fd517210524d9f9fdc1e89bd47bafc4fcf5a8e
wanti12 <wantiwanti156@gmail.com>
2023-10-12 19:58:30 +0700

    Days4_inputan

---
+++ Days4inputann.java
@@ -0,0 +1,22 @@
+package days4inputann;
+
+import java.util.Scanner;
+
+public class Days4inputann {
+
+    public static void main(String[] args) {
+        Scanner inputWa = new Scanner(System.in);
+
+        String nama;
+        String kelas;
+        String nim;
+
+        System.out.print("nama : ");
+        nama = inputWa.nextLine();
+        System.out.print("kelas : ");
+        kelas = inputWa.nextLine();
+        System.out.print("nim : ");
+        nim = inputWa.nextLine();
+    }
+
+}
