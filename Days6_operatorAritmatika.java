5c61660164ba74bfc695b256ef030fb2def9fc86
wanti12 <wantiwanti156@gmail.com>
2023-10-14 17:43:47 +0700

    Days6_operasiAritmatika

---
+++ Days6_operatorAritmatika.java
@@ -0,0 +1,23 @@
+package days6_operator.aritmatika;
+
+public class Days6_operatorAritmatika {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 2;
+
+        int hasil1 = a + b;
+        int hasil2 = a - b;
+        int hasil3 = a * b;
+        int hasil4 = a / b;
+        int hasil5 = a % b;
+
+        System.out.println("hasil penjumlahan = " + hasil1);
+        System.out.println("hasil pengurangan = " + hasil2);
+        System.out.println("hasil perkalian   = " + hasil3);
+        System.out.println("hasil pembagian   = " + hasil4);
+        System.out.println("hasil sisa bagi   = " + hasil5);
+    }
+
+}
