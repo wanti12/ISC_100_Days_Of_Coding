dd88464de2016ee1c9bdd08e9f97da3f2277608f
wanti12 <wantiwanti156@gmail.com>
2023-10-15 19:44:52 +0700

    Days7_Penugasan

---
+++ Days7Penugasan.java
@@ -0,0 +1,20 @@
+
+package days7penugasan;
+
+
+public class Days7Penugasan {
+
+    
+    public static void main(String[] args) {
+        String a = "operator penugasan";
+        int b = 12;
+        int c = 3;
+        b += 3;
+        System.out.println(b);
+        System.out.println(b -= c);
+        System.out.println(b *= c);
+        System.out.println(b /= c);
+        System.out.println(b %= c);
+    }
+
+}
