1390267c3edab81150a5e383866d7a61882f2397
wanti12 <wantiwanti156@gmail.com>
2023-10-26 21:01:34 +0700

    Days18_nestedif

---
+++ Days18_NestedIf.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication28;
+
+/**
+ *
+ * @author Wanti
+ */
+public class JavaApplication28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("menentukan nilai ");
+
+        byte a = 84;
+        byte b = 85;
+
+        if (a == 84) {
+            if (b == 85) {
+                System.out.println("nilai 1 mendapatkan nilai 84 dan nilai 2 mendapatkan nilai 85");
+            } else {
+                System.out.println("1 nilai 84 dan 2 bukan nilai 85");
+            }
+        } else {
+            System.out.println("1 bukan nilai 84 dan  2 bukan nilai 85 atau 2 adalah nilai 85");
+
+        }
+
+    }
+
+}
