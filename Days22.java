02bbbf81567598b3ddfcae2ac1562f8d525a283f
Wanti <147162518+wanti12@users.noreply.github.com>
2023-10-30 22:26:23 +0700

    Update Days22.java

--- Days22.java
+++ Days22.java
@@ -12,11 +12,9 @@ public class Days22 {
 
         if (x % 2 == 0 && x != 0) {
             y = x + 2;
-            System.out.println(x + " adalah bilangan genap maka ditambah 2 \n jadi nilai x + 2 = " + y);
             System.out.println(y + " adalah bilangan genap");
         } else if (x % 2 != 0) {
             y = x + 1;
-            System.out.println(x + " adalah bilangan ganjil maka ditambah 1 \n jadi nilai x + 1 = " + y);
             System.out.println(y + " adalah bilangan genap");
         } else {
             System.out.println("bukan bilangan genap dan ganjil");
