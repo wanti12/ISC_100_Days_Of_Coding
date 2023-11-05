d83be49b5170c208c68603fb34805f1685c7b444
wanti12 <wantiwanti156@gmail.com>
2023-11-05 20:33:32 +0700

    Days28_LoopBersarang

---
+++ Days28_LoopBersarang.java
@@ -0,0 +1,16 @@
+package Days28_LoopBersarang;
+
+public class Days28_LoopBersarang {
+
+    public static void main(String[] args) {
+        System.out.println(" membuat kotak persegi");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
