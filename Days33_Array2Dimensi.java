885f673c4ed9490143d2349b11a858d782885a7c
wanti12 <wantiwanti156@gmail.com>
2023-11-10 22:15:30 +0700

    Days33_Array2Dimensi

---
+++ Days33_Array2Dimensi.java
@@ -0,0 +1,22 @@
+package Days33_Array2Dimensi;
+
+import java.util.Arrays;
+
+public class Days33_Array2Dimensi {
+
+    public static void main(String[] args) {
+        Integer[][] b = {
+            {60, 70, 66, 80},
+            {90, 88, 76, 77},
+            {88, 78, 68, 80},};
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+        System.out.println(b[i][j]);
+
+            }
+
+        }
+
+    }
+}
