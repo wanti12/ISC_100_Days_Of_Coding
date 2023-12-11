06abd67ecdd4ddcf7d045e8a3dcf76a95b690c00
wanti12 <wantiwanti156@gmail.com>
2023-12-11 19:03:56 +0700

    Days64

---
+++ Days64.java
@@ -0,0 +1,32 @@
+package Days64;
+
+import java.util.*;
+
+public class Days64 {
+
+    public static void main(String[] args) {
+        String a = "*";
+        String b = "* * *";
+        String c = "* * * * * * * *";
+        String d = "* * * *";
+        String e = "* * * * * *";
+        System.out.printf("%8s" + " \n", a);
+        System.out.printf("%10s" + " \n", b);
+        //System.out.printf("%11s" + " \n", d);
+        System.out.printf("%13s" + " \n", e);
+        System.out.printf("%s" + " \n", c);
+        System.out.printf("%11s" + " \n", d);
+        System.out.printf("%13s" + " \n", e);
+        System.out.printf("%s" + " \n", c);
+        System.out.printf("%11s" + " \n", d);
+        System.out.printf("%13s" + " \n", e);
+        System.out.printf("%s" + " \n", c);
+        System.out.printf("%10s" + " \n", b);
+        System.out.printf("%10s" + " \n", b);
+        System.out.printf("%10s" + " \n", b);
+
+        System.out.println();
+
+    }
+
+}
